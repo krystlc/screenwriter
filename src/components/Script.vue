91f5fcd4db80316d77bc2927dae6e664ede683b9
krystlc <alex.valle@gmail.com>
2018-04-14 20:16:47 -0400

    added delete function

--- src/components/Script.vue
+++ src/components/Script.vue
@@ -4,16 +4,16 @@
       <h1 class="title has-text-centered">{{ doc.title }}</h1>
       <h6>Fade in:</h6>
       <template v-for="(el, index) in doc.script">
-        <h6 v-if="el.element === 'slugline'" :key="index" :id="`el-${index}`">{{ el.fields.position }} {{ el.fields.location }} - {{ el.fields.time }}</h6>
-        <p v-else-if="el.element === 'action'" :key="index" :id="`el-${index}`">{{ el.fields.content }}</p>
-        <blockquote v-else-if="el.element === 'dialogue'" :key="index" :id="`el-${index}`">
+        <h6 v-if="el.element === 'slugline'" :key="index" @click="editField(index)">{{ el.fields.position }} {{ el.fields.location }} - {{ el.fields.time }}</h6>
+        <p v-else-if="el.element === 'action'" :key="index" @click="editField(index)">{{ el.fields.content }}</p>
+        <blockquote v-else-if="el.element === 'dialogue'" :key="index" @click="editField(index)">
           <header class="has-text-centered">
             <h6>{{ el.fields.character }} <span v-if="el.fields.extention">({{ el.fields.extention }})</span></h6>
             <em v-if="el.fields.paranthetical.length">({{ el.fields.paranthetical }})</em>
           </header>
           <p>{{ el.fields.content }}</p>
         </blockquote>
-        <h6 class="has-text-right" v-else-if="el.element === 'transition'" :key="index" :id="`el-${index}`">{{ el.fields.transition }}:</h6>
+        <h6 class="has-text-right" v-else-if="el.element === 'transition'" :key="index" @click="editField(index)">{{ el.fields.transition }}:</h6>
       </template>
     </main>
     <section class="hero is-fullheight">
@@ -21,7 +21,7 @@
         <div class="container">
           <h1 class="title">Screenwriter</h1>
           <h2 class="subtitle">A bad hombre project</h2>
-          <element-form @payload="payload"></element-form>
+          <element-form @payload="payload" @delete="deleteField" :edit="element"></element-form>
         </div>
       </div>
     </section>
@@ -36,17 +36,39 @@ export default {
   components: { ElementForm },
   data () {
     return {
-      doc: DemoScript
+      doc: DemoScript,
+      element: {}
     }
   },
   methods: {
     editField (index) {
-      console.log('editing function coming soon!', this.doc.script[index])
+      this.element = {
+        index,
+        fieldset: this.doc.script[index]
+      }
+    },
+    deleteField (index) {
+      this.doc.script.splice(index++, 1)
     },
     payload (fieldset) {
-      this.doc.script.push(fieldset)
-      document.getElementById('container').scrollIntoView(false)
+      if (fieldset.index !== '') {
+        this.doc.script[fieldset.index] = fieldset.data
+      } else {
+        this.doc.script.push(fieldset.data)
+        document.getElementById('container').scrollIntoView(false)
+      }
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+main {
+  a {
+    display: none
+  }
+  *:hover a {
+    display: inline-block
+  }
+}
+</style>
--- src/components/forms/ElementForm.vue
+++ src/components/forms/ElementForm.vue
@@ -1,43 +1,47 @@
 <template>
-  <form @submit.prevent="sendForm">
+  <div>
     <div class="field has-addons">
       <div class="control" v-for="form in forms" :key="form">
         <button class="button" v-shortkey.once="[form.charAt(0)]" @shortkey="loadForm(form)" @click.prevent="loadForm(form)">{{ form }}</button>
       </div>
     </div>
-    <template v-if="displayForm === 'slugline'">
-      <div class="field has-addons">
-        <element-field :config="{label: 'position'}" v-model="fields.position"></element-field>
-        <element-field :config="{label: 'location', isExpanded: true, isRequired: true}" v-model="fields.location"></element-field>
-        <element-field :config="{label: 'time'}" v-model="fields.time"></element-field>
+    <form @submit.prevent="validateForm">
+      <template v-if="displayForm === 'slugline'">
+        <div class="field has-addons">
+          <element-field :config="{label: 'position'}" v-model="fields.position" ref="focus"></element-field>
+          <element-field :config="{label: 'location', isExpanded: true, isRequired: true}" v-model="fields.location"></element-field>
+          <element-field :config="{label: 'time'}" v-model="fields.time"></element-field>
+        </div>
+      </template>
+      <template v-if="displayForm === 'action'">
+        <div class="field">
+          <element-field :config="{label: 'action', tag: 'textarea', isRequired: true}" v-model="fields.content" ref="focus"></element-field>
+        </div>
+      </template>
+      <template v-if="displayForm === 'dialogue'">
+        <div class="field has-addons">
+          <element-field :config="{label: 'character', isExpanded: true, isRequired: true}" v-model="fields.character" ref="focus"></element-field>
+          <element-field :config="{label: 'extention'}" v-model="fields.extention"></element-field>
+          <element-field :config="{label: 'paranthetical'}" v-model="fields.paranthetical"></element-field>
+        </div>
+        <div class="field">
+          <element-field :config="{label: 'speech', tag: 'textarea', isRequired: true}" v-model="fields.content"></element-field>
+        </div>
+      </template>
+      <template v-if="displayForm === 'transition'">
+        <div class="field">
+          <element-field :config="{label: 'transition', isRequired: true}" v-model="fields.transition" ref="focus"></element-field>
+        </div>
+      </template>
+      <div class="field" v-if="displayForm">
+        <div class="control">
+          <button class="button is-primary">ctrl+enter</button>
+          <button class="button is-danger" @click.prevent="handleDelete" v-if="index !== ''">delete</button>
+          <button class="button" disabled="disabled" v-shortkey="['ctrl','space']" @shortkey="focusField">ctrl+space</button>
+        </div>
       </div>
-    </template>
-    <template v-if="displayForm === 'action'">
-      <div class="field">
-        <element-field :config="{label: 'action', tag: 'textarea', isRequired: true}" v-model="fields.content"></element-field>
-      </div>
-    </template>
-    <template v-if="displayForm === 'dialogue'">
-      <div class="field has-addons">
-        <element-field :config="{label: 'character', isExpanded: true, isRequired: true}" v-model="fields.character"></element-field>
-        <element-field :config="{label: 'extention'}" v-model="fields.extention"></element-field>
-        <element-field :config="{label: 'paranthetical'}" v-model="fields.paranthetical"></element-field>
-      </div>
-      <div class="field">
-        <element-field :config="{label: 'speech', tag: 'textarea', isRequired: true}" v-model="fields.content"></element-field>
-      </div>
-    </template>
-    <template v-if="displayForm === 'transition'">
-      <div class="field">
-        <element-field :config="{label: 'transition', isRequired: true}" v-model="fields.transition"></element-field>
-      </div>
-    </template>
-    <div class="field" v-if="displayForm">
-      <div class="control">
-        <button class="button is-primary">ctrl+enter</button>
-      </div>
-    </div>
-  </form>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -46,10 +50,12 @@ import ElementField from './ElementField'
 const forms = ['slugline', 'action', 'dialogue', 'transition']
 
 export default {
+  props: ['edit'],
   components: { ElementField },
   data () {
     return {
       forms,
+      index: '',
       fields: {
         position: '',
         location: '',
@@ -64,35 +70,56 @@ export default {
     }
   },
   methods: {
+    clearFields () {
+      const fields = this.fields
+      Object.keys(fields).forEach(v => { fields[v] = '' })
+      this.index = ''
+    },
     loadForm (form) {
-      this.clearForm()
+      this.clearFields()
       this.displayForm = form
     },
     clearForm () {
-      const fields = this.fields
-      Object.keys(fields).forEach(v => { fields[v] = '' })
+      this.displayForm = ''
     },
-    sendForm () {
-      const fielset = {
-        element: this.displayForm,
-        fields: {
-          position: this.fields.position,
-          location: this.fields.location,
-          time: this.fields.time,
-          content: this.fields.content,
-          character: this.fields.character,
-          extention: this.fields.extention,
-          paranthetical: this.fields.paranthetical,
-          transition: this.fields.transition
+    validateForm () {
+      const fieldset = {
+        index: this.index,
+        data: {
+          element: this.displayForm,
+          fields: {
+            position: this.fields.position,
+            location: this.fields.location,
+            time: this.fields.time,
+            content: this.fields.content,
+            character: this.fields.character,
+            extention: this.fields.extention,
+            paranthetical: this.fields.paranthetical,
+            transition: this.fields.transition
+          }
         }
       }
-      Object.keys(fielset.fields).forEach((key) => (fielset.fields[key] === '') && delete fielset.fields[key])
-      this.$emit('payload', fielset)
-      this.displayForm = ''
+      Object.keys(fieldset.data.fields).forEach((key) => (fieldset.data.fields[key] === '') && delete fieldset.data.fields[key])
+      this.sendForm(fieldset)
+    },
+    sendForm (fieldset) {
+      this.$emit('payload', fieldset)
       this.clearForm()
     },
-    test () {
-      console.log('wee')
+    focusField () {
+      this.$refs.focus.$el.childNodes[0].focus()
+    },
+    handleDelete () {
+      this.$emit('delete', this.index)
+      this.clearFields()
+      this.clearForm()
+    }
+  },
+  watch: {
+    edit: function (data) {
+      this.index = data.index
+      this.displayForm = data.fieldset.element
+      this.fields = data.fieldset.fields
     }
   }
 }
